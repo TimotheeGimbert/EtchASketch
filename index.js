184029646db84d03b1f4f1d612760d4c2b023720
TimotheeGimbert <timothee.gimbert@free.fr>
2021-09-26 00:07:30 +0200

    reset

--- index.js
+++ index.js
@@ -1,62 +1,31 @@
-///////////////
-// VARIABLES //
-///////////////
+let boxPerLine = 16;
+const container = document.querySelector('#container');
+const clearButton = document.querySelector('#clearButton');
 
-let lineDensity = 16;
-
-
-///////////////
-// FUNCTIONS //
-///////////////
-
-function setGrid(n) {
-    // creates a grid of n*n divs
+function createGrid(n) {
     for (let i = 1; i <= n; i++) {
-        const line = document.createElement('section'); // grid lines are sections
+        const line = document.createElement('section'); 
         for (let i = 1; i <= n; i++) {
-            const div = document.createElement('div'); // grid elements are divs
+            const div = document.createElement('div');
             line.appendChild(div);
         }
         container.appendChild(line);
     }
 }
 
-function getLineWidth() {
-    // calculates the width of the current grid, and returns it
-    const width = lineDensity * document.querySelector('section > div').offsetWidth;
-    return width;
-}
 
-function getNewDensity() {
-    // prompts for the number of boxes per line for the new grid, and returns it
-    const newDensity = prompt("How many boxes per line for the new grid?");
-    return newDensity;
-}
+createGrid(boxPerLine);
 
-function getNewBoxWidth() {
-    // calculate the new box width based on the new density, and returns it
-    const newBoxWidth = getLineWidth() / getNewDensity();
-    return newBoxWidth;
-}
-
-
-///////////////
-// EXECUTION //
-///////////////
-
-const container = document.querySelector('#container');
-setGrid(lineDensity);
-
-let boxes = document.querySelectorAll('section > div');
+let boxes = document.querySelectorAll('#container div');
 boxes.forEach(box => {
     box.addEventListener('mouseover', () => {
-        box.style.cssText = "background-color: black;";
+        box.style.backgroundColor = "black";
     });
 });
 
-let clearButton = document.querySelector('button');
+
 clearButton.addEventListener('click', () => {
     boxes.forEach(box => {
-        box.style.cssText = "background-color: white;";
+        box.style.backgroundColor = "white";
     });
 });

719f7177758234b8dcc21bd74b8255f06604b9a8
TimotheeGimbert <timothee.gimbert@free.fr>
2021-09-25 15:45:27 +0200

    Add button Clear in html and simplify query selector all

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ for (let i = 1; i <= 16; i++) { // 16 lines in the grid
     const line = document.createElement('section'); // lines are sections
     for (let i = 1; i <= 16; i++) { // 16 boxes in each line of the grid
         const div = document.createElement('div'); // boxes are divs
-        div.classList.add('box'); // boxes receive .box class
         line.appendChild(div);
     }
     container.appendChild(line);
 }
 
-const boxes = document.querySelectorAll('.box');
+const boxes = document.querySelectorAll('section > div');
 boxes.forEach(box => {
     box.addEventListener('mouseover', () => {
         box.style.cssText = "background-color: black;";

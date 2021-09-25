4ece3694cdef8731ea7ec49483223848c4a3f5f6
TimotheeGimbert <timothee.gimbert@free.fr>
2021-09-25 18:33:19 +0200

    Reorganize code

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-const container = document.querySelector('#container');
+///////////////
+// VARIABLES //
+///////////////
 
-for (let i = 1; i <= 16; i++) { // 16 lines in the grid
-    const line = document.createElement('section'); // lines are sections
-    for (let i = 1; i <= 16; i++) { // 16 boxes in each line of the grid
-        const div = document.createElement('div'); // boxes are divs
-        line.appendChild(div);
+let boxesPerLine = 16;
+
+
+///////////////
+// FUNCTIONS //
+///////////////
+
+function setGrid(n) {
+    for (let i = 1; i <= n; i++) { // 16 initial lines
+        const line = document.createElement('section'); // lines are sections
+        for (let i = 1; i <= n; i++) { // 16 initial boxes
+            const div = document.createElement('div'); // boxes are divs
+            line.appendChild(div);
+        }
+        container.appendChild(line);
     }
-    container.appendChild(line);
 }
 
-const boxes = document.querySelectorAll('section > div');
+function newGrid() {
+    const initialGridWidth = 16*document.querySelector('section > div').offsetWidth;
+    const boxesPerLine = prompt("How many boxes per line for the new grid?");
+    const newBoxWidth = initialGridWidth / boxesPerLine;
+    return newBoxWidth
+}
+
+
+///////////////
+// EXECUTION //
+///////////////
+
+const container = document.querySelector('#container');
+setGrid(boxesPerLine);
+
+let boxes = document.querySelectorAll('section > div');
 boxes.forEach(box => {
     box.addEventListener('mouseover', () => {
         box.style.cssText = "background-color: black;";
     });
 });
 
-const clearButton = document.querySelector('button');
+let clearButton = document.querySelector('button');
 clearButton.addEventListener('click', () => {
     boxes.forEach(box => {
         box.style.cssText = "background-color: white;";

f9938bb66d3511c1647f33c7d257b74297011019
TimotheeGimbert <timothee.gimbert@free.fr>
2021-09-25 13:18:05 +0200

    Add eventlistener coloring boxes

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
+const container = document.querySelector('#container');
 
-function setLine() {
-    // Create a line composed of 16 divs
-    const line = document.createElement('section'); // creates a line <section>   
-    for (let i = 1; i <= 16; i++) {
-        const div = document.createElement('div'); // creates a <div>
-        line.appendChild(div); // adds 16 divs to the line
+for (let i = 1; i <= 16; i++) { // 16 lines in the grid
+    const line = document.createElement('section'); // lines are sections
+    for (let i = 1; i <= 16; i++) { // 16 boxes in each line of the grid
+        const div = document.createElement('div'); // boxes are divs
+        div.classList.add('box'); // boxes receive .box class
+        line.appendChild(div);
     }
-    container.appendChild(line); // adds the line to #container
+    container.appendChild(line);
 }
 
-// creates 16 lines 
-for (let i = 1; i <= 16; i++) {
-    setLine();
-}
-
-
-
-
-
-
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(box => {
+    box.addEventListener('mouseover', () => {
+        box.style.cssText = "background-color: black;";
+    });
+});
 

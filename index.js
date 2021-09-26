ae417d870640ac6787e8841cafec124f3e634194
TimotheeGimbert <timothee.gimbert@free.fr>
2021-09-26 13:37:50 +0200

    Adding gridButton and js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const container = document.getElementById('container');
 const clearButton = document.getElementById('clearButton');
+const gridButton = document.getElementById('gridButton');
 
-function getBoxWidth(n) {
+function getBoxWidth(n) { // calculates box width depending on the number of boxes per line, and returns it
     const containerSize = container.offsetWidth;
     const boxSize = containerSize / n;
     return boxSize;
 }
 
-function createGrid(n) {
+function createGrid(n) { // creates n rows of n boxes
     let size = getBoxWidth(n) + "px";
     for (let i = 1; i <= n; i++) {
         let row = document.createElement('section');
@@ -23,24 +24,37 @@ function createGrid(n) {
     }
 }
 
-
-createGrid(16);
-
-let boxes = document.querySelectorAll('.box');
-boxes.forEach(box => {
+function colorGrid() { // eventListener that colors boxes in black when hovered
+    let boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
     box.addEventListener('mouseover', () => {
         box.style.backgroundColor = "black";
+        });
     });
-});
+}
+
+function askGridDensity() { // prompts the user to choose new grid density, and returns it
+    let newGridDensity = prompt("How many boxes per line for the new grid?");
+    return newGridDensity;
+}
+
+//////////
+// MAIN //
+//////////
+
+createGrid(16);
+colorGrid();
 
 clearButton.addEventListener('click', () => {
+    let boxes = document.querySelectorAll('.box');
     boxes.forEach(box => {
         box.style.backgroundColor = "white";
     });
 });
 
 gridButton.addEventListener('click', () => {
-    container.forEach(child => {
-        container.removeChild(child);
-    });
+    while (container.firstChild) container.removeChild(container.firstChild);
+    let newGridDensity = askGridDensity();
+    createGrid(newGridDensity);
+    colorGrid();
 });
